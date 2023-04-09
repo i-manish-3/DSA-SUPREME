6d3658049a8cc9acaba6df95cbbd2887598a0eb4
Manish <official.iamanish@gmail.com>
2023-04-09 12:16:05 +0530

    code

---
+++ Linked List/creation.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+class node{
+    public:
+
+    int data;
+    node* next;
+    
+    node(int d)
+    {
+        this->data = d;
+        this->next = NULL;
+    }
+};
+
+
+int main(){
+    
+    return 0;
+}
