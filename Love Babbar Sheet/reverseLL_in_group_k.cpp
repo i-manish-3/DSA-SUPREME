f6c0c9c7dbdfc89291f59cc65cbf6c7912501459
Manish <official.iamanish@gmail.com>
2023-04-10 01:44:01 +0530

    code

---
+++ Love Babbar Sheet/reverseLL_in_group_k.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        if(head == NULL)
+        {
+            return NULL;
+        }
+        
+        node* curr = head;
+        node* prev = NULL, *nextNode = NULL;
+        
+        int count = 0;
+        
+        while(curr != NULL && count < k)
+        {
+            nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+            count++;
+        }
+        
+        if(curr != NULL)
+        {
+            head->next = reverse(curr, k);
+        }
+        
+        return prev;
+    }
+};
+
+int main(void)
+{
+    int t;
+    cin>>t;
+     
+    while(t--)
+    {
+        struct node* head = NULL;
+        struct node* temp = NULL;
+        int n;
+        cin >> n;
+         
+        for(int i=0 ; i<n ; i++)
+        {
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+        }
+        
+        int k;
+        cin>>k;
+        
+        Solution ob;
+        head = ob.reverse(head, k);
+        printList(head);
+    }
+     
+    return(0);
+}
